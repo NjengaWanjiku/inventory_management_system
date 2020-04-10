1c9da06ced8f5e867ad6c76ed920945326c3bde1
GreyTea <njengaewanjiku@gmail.com>
2020-04-10 14:18:37 +0300

    line/pie graphs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 
 import pygal
-
+import psycopg2
 # specific
 # from pygal import pie
 
@@ -133,38 +133,99 @@ def edit_inventory():
 
 @app.route('/data_visualisation')
 def data_visualisation():
+    # conn = psycopg2.connect("dbname='postgres' user= 'postgres' host='localhost' port='5432' password='kiprugut'")
+
+    # cur = conn.cursor()
+    # cur.execute(""" 
+    # SELECT type,count(type)
+	# FROM public.inventories
+	
+	# GROUP BY type
+    
+    # """)
+    # product_service = cur.fetchall()
+
+    # print(product_service)
 
     # initializing your pie chart
+    
     pie_chart= pygal.Pie()
+    my_pie_data = [
+        ('Nairobi',40),
+        ('Kilifi' ,10),
+        ('Machakos' ,10),
+        ('Kiambu' ,20),
+        ('Nakuru',20)
+        ]
 
+    
+    for each in my_pie_data:
+        pie_chart.add(each[0],each[1])
 
-
+        pie_data = pie_chart.render_data_uri()     
+    pie_chart.title ='Total Sales'
+    print(my_pie_data)
     # add components to your pie chart
         # 1.add title
         # 2.partition your pie chart
-    pie_chart.title ='RONA IN KENYA'
-    pie_chart.add('Nairobi',40)
-    pie_chart.add('Kilifi',10)
-    pie_chart.add('Machakos',10)
-    pie_chart.add('Kiambu',20)
-    pie_chart.add('Nakuru',20)
+
     
-    pie_data = pie_chart.render_data_uri()
-    # return pie_chart.render()
+    # pie_chart.add('Nairobi',40)
+    # pie_chart.add('Kilifi',10)
+    # pie_chart.add('Machakos',10)
+    # pie_chart.add('Kiambu',20)
+    # pie_chart.add('Nakuru',20)
     # end of piechart
 
     # start of line_chart
-    line_chart = pygal.StackedLine(fill=True)
+    # cur.execute("""
+    
+    
+    
+    # """)
 
-    line_chart.title = 'DATA USAGE'
-    line_chart.x_labels =  map(str,range(2002,2013))
-    line_chart.add('Firefox',[None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome', [.3, .9, 17.1, 15.3, .6, .5, 1.6])
-    line_chart.add('IE', [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',[14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-   
-    line_data = line_chart.render_data_uri()
-    return render_template('charts.html', pie=pie_data, line=line_data)
+    #  monthly_sales=cur.fetchall()
+    #  print(monthly_sales)
+
+    data = [
+         {'month':'January','total':22},
+         {'month':'February','total':27},
+         {'month':'March','total':23},
+         {'month':'April','total':20},
+         {'month':'May','total':12},
+         {'month':'June','total':32},
+         {'month':'July','total':42},
+         {'month':'August','total':72},
+         {'month':'September','total':52},
+         {'month':'October','total':42},
+         {'month':'November','total':92},
+         {'month':'December','total':102}
+     ]
+    a=[]
+    b=[]
+    for each in data:
+
+        x=each["month"]
+        y=each["total"]
+        a.append(x)
+        b.append(y)
+
+
+        line_chart = pygal.Line()
+        line_chart.title = 'MONTHLY SALES'
+        line_chart.x_label = a
+        line_chart.add('MonthlySales',b)
+        print(data)
+         
+
+    # line_chart.add('Firefox',[None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    # line_chart.add('Chrome', [.3, .9, 17.1, 15.3, .6, .5, 1.6])
+    # line_chart.add('IE', [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    # line_chart.add('Others',[14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+
+        line_data = line_chart.render_data_uri()
+
+        return render_template('charts.html',chart=pie_data,line=line_data)
 
 #  run_your_app
 if __name__=="__main__":
